240108300b35116811b1af3c6145985215b6433a
Calvin Danyalson <calvin@coding.id>
2024-09-28 11:38:03 +0700

    changed: restructure repository folder and projects

---
+++ 01-spring-boot-overview/02-dev-tools-demo/src/test/java/com/springboot_1/demo/mydemoapp/MydemoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot_1.demo.mydemoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MydemoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

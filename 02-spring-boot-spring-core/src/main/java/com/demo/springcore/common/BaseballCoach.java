05cb3255a6c20d448fb366afc9ef967b371bcfad
Calvin Danyalson <119773802+calvindany@users.noreply.github.com>
2024-09-29 16:22:10 +0700

    added: primary annotation to one of the coach component
    Notes:
    @Primary annotation used to tell spring that the component that use @Primary is the default component used when in constuctor injection is not using @Qualifier

--- 02-spring-boot-spring-core/src/main/java/com/demo/springcore/common/BaseballCoach.java
+++ 02-spring-boot-spring-core/src/main/java/com/demo/springcore/common/BaseballCoach.java
@@ -1,11 +1,13 @@
 package com.demo.springcore.common;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BaseballCoach implements ICoach{
 
     @Override
+    @Primary
     public String getDailyWorkOut() {
         return "Spend 30 menutes in battling practice";
     }

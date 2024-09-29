7e7a2c0c86943c57f92c9beec23710abcda5e027
Calvin Danyalson <119773802+calvindany@users.noreply.github.com>
2024-09-29 16:19:52 +0700

    fixed: add component annotation on Track and Tennis Coach

--- 02-spring-boot-spring-core/src/main/java/com/demo/springcore/common/TennisCoach.java
+++ 02-spring-boot-spring-core/src/main/java/com/demo/springcore/common/TennisCoach.java
@@ -1,5 +1,8 @@
 package com.demo.springcore.common;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class TennisCoach implements ICoach{
     @Override
     public String getDailyWorkOut() {
--- 02-spring-boot-spring-core/src/main/java/com/demo/springcore/common/TrackCoach.java
+++ 02-spring-boot-spring-core/src/main/java/com/demo/springcore/common/TrackCoach.java
@@ -1,5 +1,8 @@
 package com.demo.springcore.common;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class TrackCoach implements ICoach{
     @Override
     public String getDailyWorkOut() {

8a4909467a0fe601e0498e6566c66a9ae5e4ddce
Calvin Danyalson <119773802+calvindany@users.noreply.github.com>
2024-10-13 17:28:22 +0700

    added: default student entity mapping to student table

---
+++ 03-spring-boot-hibernate-jpa-crud/src/main/java/com/example/demoapp/entity/Student.java
@@ -0,0 +1,75 @@
+package com.example.demoapp.entity;
+
+import jakarta.persistence.*;
+
+import javax.annotation.processing.Generated;
+
+@Entity
+@Table(name="student")
+public class Student {
+
+    // Define field column mapping
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "first_name")
+    private String first_name;
+
+    @Column(name = "last_name")
+    private String last_name;
+
+    @Column(name = "email")
+    private String email;
+
+    // Define constructor
+    public Student(String first_name, String last_name, String email) {
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.email = email;
+    }
+
+    // Define setter & getter
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return first_name;
+    }
+
+    public void setFirstName(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLastName() {
+        return last_name;
+    }
+
+    public void setLastName(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", first_name='" + first_name + '\'' +
+                ", last_name='" + last_name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}

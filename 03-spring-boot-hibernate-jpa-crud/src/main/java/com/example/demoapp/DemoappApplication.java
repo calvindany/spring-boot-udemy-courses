29d53629b82981115309d95c9653fff608bab56e
calvindany <calvindany1102@gmail.com>
2024-10-19 08:11:04 +0700

    added: new function for creating multiply student data

--- 03-spring-boot-hibernate-jpa-crud/src/main/java/com/example/demoapp/DemoappApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/src/main/java/com/example/demoapp/DemoappApplication.java
@@ -16,8 +16,30 @@ public class DemoappApplication {
 	@Bean
 	public CommandLineRunner commandLineRunner(StudentDAO studentDAO){
 		return runner -> {
-			createStudent(studentDAO);
+//			createStudent(studentDAO);
+			createMultipleStudent(studentDAO);
 		};
+	}
+
+	public void createMultipleStudent(StudentDAO studentDAO) {
+		System.out.println("Starting Insert 3 Student Data Process");
+
+		Student tempStudent1 = new Student("John", "Doe", "johndoe@mail.com");
+		Student tempStudent2 = new Student("Benita", "Applebum", "benita@mail.com");
+		Student tempStudent3 = new Student("Mary", "Public", "maryy@mail.com");
+
+		System.out.println("Saving student data 1");
+		studentDAO.save(tempStudent1);
+		System.out.println("Student data is saved, ID: " + tempStudent1.getId());
+
+		System.out.println("Saving student data 2");
+		studentDAO.save(tempStudent2);
+		System.out.println("Student data is saved, ID: " + tempStudent2.getId());
+
+		System.out.println("Saving student data 3");
+		studentDAO.save(tempStudent3);
+		System.out.println("Student data is saved, ID: " + tempStudent3.getId());
+
 	}
 
 	private void createStudent(StudentDAO studentDAO) {
@@ -28,6 +50,6 @@ public class DemoappApplication {
 		System.out.println("Saving student data");
 		studentDAO.save(tempStudent);
 
-		System.out.println();
+		System.out.println("Student data is saved, ID: " + tempStudent.getId());
 	}
 }

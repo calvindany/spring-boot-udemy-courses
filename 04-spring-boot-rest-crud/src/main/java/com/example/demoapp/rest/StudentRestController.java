1d8eaacac29091861f0af73658365ac568522157
Calvin Danyalson <119773802+calvindany@users.noreply.github.com>
2024-10-20 16:28:37 +0700

    changed: handleException on student data to global exception handler
    Notes:
    To do this, we use @ControllerAdvice as a new class

--- 04-spring-boot-rest-crud/src/main/java/com/example/demoapp/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/src/main/java/com/example/demoapp/rest/StudentRestController.java
@@ -41,26 +41,4 @@ public class StudentRestController {
 
         return students.get(studentId);
     }
-
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException (StudentNotFoundException err) {
-        StudentErrorResponse error = new StudentErrorResponse();
-
-        error.setStatus(HttpStatus.NOT_FOUND.value());
-        error.setMessage(err.getMessage());
-        error.setTimestamp(System.currentTimeMillis());
-
-        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException (Exception err) {
-        StudentErrorResponse error = new StudentErrorResponse();
-
-        error.setStatus(HttpStatus.BAD_REQUEST.value());
-        error.setMessage(err.getMessage());
-        error.setTimestamp(System.currentTimeMillis());
-
-        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
-    }
 }
